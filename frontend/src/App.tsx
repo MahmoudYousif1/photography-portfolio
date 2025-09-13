47e271323de1ef6e71c31bd2cd7e45c3c6423995
moudy <yothesif@gmail.com>
2025-09-13 16:04:47 +0100

    Implemented page transition animation with framer-motion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,60 @@
 import "./App.css";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Home from "./components/pages/Home";
 import Gallery from "./components/pages/Gallery";
 import About from "./components/pages/About";
 import Loader from "./components/utils/Loader";
+import PageTransition from "./components/utils/PageTransition";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <PageTransition>
+              <Home />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/gallery"
+          element={
+            <PageTransition>
+              <Gallery />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/gallery/:category"
+          element={
+            <PageTransition>
+              <Gallery />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PageTransition>
+              <About />
+            </PageTransition>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,13 +70,7 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/gallery/:category" element={<Gallery />} />
-          <Route path="/about" element={<About />}
-          />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </Router>
   );
